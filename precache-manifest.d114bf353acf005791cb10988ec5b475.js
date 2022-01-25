cf852eb136d39b6c689fbbfe51c9a105bba277cc
AjayS1509 <ajaypsoni1509@gmail.com>
2022-01-25 22:32:26 +0530

    Updates

---
+++ precache-manifest.d114bf353acf005791cb10988ec5b475.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba5450e342f0427b6e4977160f870d48",
+    "url": "/box_office/index.html"
+  },
+  {
+    "revision": "a9d27f499dcdc50c3366",
+    "url": "/box_office/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "3c5c767c0c06e528bc62",
+    "url": "/box_office/static/js/2.f8efd8a8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/box_office/static/js/2.f8efd8a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9d27f499dcdc50c3366",
+    "url": "/box_office/static/js/main.a5d69304.chunk.js"
+  },
+  {
+    "revision": "331824223a9f83006b93",
+    "url": "/box_office/static/js/runtime-main.72276d6c.js"
+  }
+]);
